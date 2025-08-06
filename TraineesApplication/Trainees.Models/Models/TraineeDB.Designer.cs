e2f83cf8368bff131d6c7f56bce0bf32e60ed091
AlyMaged <alymagedmohamed@gmail.com>
2025-08-06 16:33:02 +0300

    added login page

--- TraineesApplication/Trainees.Models/Models/TraineeDB.Designer.cs
+++ TraineesApplication/Trainees.Models/Models/TraineeDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Trainees\TraineesApplication\Trainees.Models\Models\TraineeDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\IST\Documents\GitHub\ISTproject\TraineesApplication\Trainees.Models\Models\TraineeDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
